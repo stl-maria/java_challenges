b911025267cd2f5e398e997ec542d35ad569ee77
estelah <emfj.estela@gmail.com>
2024-06-25 22:06:22 -0300

    New java file

---
+++ Condições, Loops e Exceções/src/ControleSimplesDeSaques.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ControleSimplesDeSaques {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double limiteDiario = scanner.nextDouble(); //limite diario de saque
+
+        // Loop for para iterar sobre os saques
+        for (int i = 1; ; i++) {
+
+            double valorSaque = scanner.nextDouble(); //valor do primeiro saque
+
+            if (valorSaque == 0) {
+                System.out.println("Transacoes encerradas.");
+                break; // Encerra o loop
+            } else if (valorSaque > limiteDiario) {
+                System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+                break; // Encerra o loop
+            } else {
+                limiteDiario = limiteDiario - valorSaque;
+                System.out.println("Saque realizado. Limite restante: "+limiteDiario);
+                System.out.println("Transacoes encerradas.");
+                break;
+                // TODO: Atualizar o limite diário e imprimir a saída no formato dos exemplos.
+            }
+        }
+
+        // Fechar o scanner para evitar vazamentos de recursos
+        scanner.close();
+    }
+}
