583991be6ce054c475b1906923958fa2618d160f
estelah <emfj.estela@gmail.com>
2024-07-10 21:39:37 -0300

    Add new challenge

---
+++ Banco Digital/banco_digital/src/ContaCorrente.java
@@ -0,0 +1,22 @@
+public class ContaCorrente {
+    // Atributos
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    // Métodos
+    public void sacar(){
+
+    }
+
+    public void depositar(){
+
+    }
+
+    public void transferencia(){
+
+    }
+
+    
+
+}
